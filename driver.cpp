552f6c1c1f03e4abcd1cf11e2ada2034d8c8cb2e
Saqlain190 <saqlainrafique8@gmail.com>
2026-01-25 17:40:41 +0500

    Driver file

--- driver.cpp
+++ driver.cpp
@@ -1,4 +1,39 @@
 #include "driver.h"
-Driver::Driver(int driverId, string drivername,string loc,string z){
-    
+Driver::Driver(int driverId, string drivername,string loc,string zone){
+    id = driverId;
+    name = driverId;
+    location = loc;
+    Zone = zone;
+    available = true;
+    completedTrips = 0;
+}
+
+int Driver::getId() const{
+    return id;
+}
+
+string Driver::getName() const{
+    return name;
+}
+string Driver::getLocation() const{
+    return location;
+}
+string Driver::getZone() const{
+    return Zone;
+}
+bool Driver::isAvailable() const{
+return available;
+}
+int Driver:: getCompletedTrips() const{
+    return completedTrips;
+}
+
+void Driver::setLocation(string loc){
+ location = loc;
+}
+void Driver::setAvailable(bool status){
+available = status;
+}
+void Driver::incrementTrips(){
+completedTrips++;
 }
---
+++ driver.h
@@ -0,0 +1,32 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+#include <string>
+using namespace std;
+
+class Driver {
+private:
+    int id;             
+    string name;       
+    string location;   
+    string Zone;        
+    bool available;    
+    int completedTrips; 
+
+public:
+    
+    Driver(int driverId, string driverName, string loc, string zone);
+
+    int getId() const;
+    string getName() const;
+    string getLocation() const;
+    string getZone() const;
+    bool isAvailable() const;
+    int getCompletedTrips() const;
+
+    void setLocation(string loc);
+    void setAvailable(bool status);
+    void incrementTrips();
+};
+
+#endif
